f2b8e52a566355ccaa8e34781772aea84851baf1
Marcos <m.vinicius.sr11@gmail.com>
2023-12-07 21:17:52 -0300

    adicionando template dashboard-v1

---
+++ Views/Shared/_MainMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MonografiasIfma.Views.Shared
+{
+    public class _MainMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
